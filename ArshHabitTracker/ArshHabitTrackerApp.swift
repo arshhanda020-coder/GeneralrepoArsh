c0f9fdfa617c41ec5793c13a4916b43509ffc183
Arsh194 <arshhanda020@gmail.com>
2026-07-28 00:44:59 -0400

    Initial Commit

---
+++ ArshHabitTracker/ArshHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArshHabitTrackerApp.swift
+//  ArshHabitTracker
+//
+//  Created by Amandeep Kaur on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ArshHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
